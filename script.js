f35e2f36f6aef70e0ebc621cc04759dca25d7be1
vishal choudhary <vishalchoudharyiitian1@gmail.com>
2024-12-30 19:06:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('.panel-all').addEventListener('click', function () {
+    const dropdown = document.querySelector('.dropdown-menu');
+    dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
+});
